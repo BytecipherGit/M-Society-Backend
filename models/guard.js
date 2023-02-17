fb9a1d44654c1ab3817f21b44692e9b590e64e96
jaya <darwadedaya882@gmail.com>
2023-02-17 16:40:57 +0530

    guard model and apis added

---
+++ models/guard.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const mongooseSoftDelete = require('soft-delete-mongoose');
+const GuardSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    address: {
+        type: String,
+        require: true,
+    },
+    shift: {
+        type: String,
+        enum: ["day", "night"],
+        default: "day",
+    },
+    age: {
+        type: Number,
+    },
+    phoneNumber: {
+        type: String,
+        require: true,
+    },
+    societyId: {
+        type: Schema.Types.ObjectId,
+        ref: "msociety_societys",
+        require: true,
+    },
+    societyAdminId: {
+        type: Schema.Types.ObjectId,
+        ref: "msociety_residentialusers",
+        require: true,
+    },
+    profileImage: {
+        type: String
+    },
+    status: {
+        type: String,
+        enum: ["active", "inactive"],
+        default: "active",
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+GuardSchema.plugin(mongooseSoftDelete, {
+    paranoid: true,
+});
+const Guard = mongoose.model("msociety_guards", GuardSchema);
+
+module.exports = Guard;
