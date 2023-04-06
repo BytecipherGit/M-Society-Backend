debed9068deec90cb2a3bc5d9d78d0cd0971f6a0
jaya <darwadedaya882@gmail.com>
2023-04-06 18:56:31 +0530

    create serive subscription model done

---
+++ models/serviceSubscription.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const mongooseSoftDelete = require('soft-delete-mongoose');
+const ServiceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    duration: {
+        type: Number,
+    },
+    type: {
+        type: String,
+    },
+    stateCount: {
+        type: String,
+    },
+    cityCount: {
+        type: String,
+    },
+    status: {
+        type: String,
+        enum: ["active", "inactive"],
+        default: "active",
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedDate: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+ServiceSchema.plugin(mongooseSoftDelete, {
+    paranoid: true,
+});
+const Service = mongoose.model("msociety_servicesubscriptions", ServiceSchema);
+
+module.exports = Service;
